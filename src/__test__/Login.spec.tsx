1e419b59d7671a50b5596b93d07307f9f9ec7b35
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-08-11 14:47:11 +0900

    feat: mock 서버 응답 설정 테스트

--- src/__test__/Login.spec.tsx
+++ src/__test__/Login.spec.tsx
@@ -4,6 +4,7 @@ import { fireEvent, render, renderHook, screen, waitFor } from "@testing-library
 import LoginPage from "../pages/LoginPage";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
+import * as nock from "nock";
 
 const queryClient = new QueryClient({
   defaultOptions: {},
@@ -38,7 +39,19 @@ describe("로그인 테스트", () => {
       </QueryClientProvider>
     );
 
-    // when
+    // when - 사용자가 로그인에 실패한다.
+
+    // 가짜 서버 응답 설정
+    /**
+     * 가짜 서버를 사용하면 서버로 요청을 보내지않고 원하는 테스트를 할 수 있음
+     */
+    nock("https://inflearn.byeongjinkang.com")
+      .post("/user/login/", {
+        username: "wrong@email.com",
+        password: "wrongPassword",
+      })
+      .reply(400, { id: "123ABC" });
+
     const wrapper = ({ children }) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     );
